adcd12d3e432ca4c72fbe73db0b1503531e7f15f
Heinrich Muralt <heinrich@supertext.ch>
2020-10-24 10:08:18 +0200

    Add withStateProvider hof

--- src/StateProvider.ts
+++ src/StateProvider.ts
@@ -4,3 +4,14 @@ export default interface StateProvider<TState> {
   readonly state: TState;
   state$: Observable<TState>;
 }
+
+export function withStateProvider<TState>(stateProvider: StateProvider<TState>) {
+  return <TTarget extends {}>(target: TTarget): TTarget & StateProvider<TState> => {
+    const extendedObject = {
+      ...target,
+      state$: stateProvider.state$
+    };
+
+    return Object.defineProperty(extendedObject, "state", { get: () => stateProvider.state });
+  };
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export * from "./StateManipulationFunctions";
 export { default as StateHandler, createStateHandler } from "./StateHandler";
-export { default as StateProvider } from "./StateProvider";
+export { default as StateProvider, withStateProvider } from "./StateProvider";
