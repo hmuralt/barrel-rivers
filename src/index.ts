31a3ae20d637c80cf28f8e45fa58385e51d567bd
Heinrich Muralt <heinrich@supertext.ch>
2020-11-25 13:12:55 +0100

    Add Set and NewValueGetter types to barrel export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export * from "./StateManipulationFunctions";
-export { default as State, state } from "./State";
+export { default as State, state, Set, NewValueGetter } from "./State";
 export { default as ValueContainer, withValueContainer } from "./ValueContainer";
