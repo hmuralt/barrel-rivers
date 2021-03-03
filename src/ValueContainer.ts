e41b610a626ee54fa59637281642f4a57da67073
Heinrich Muralt <heinrich@supertext.ch>
2021-03-03 10:36:36 +0100

    Set value$ property of ValueContainer to readonly

--- src/ValueContainer.ts
+++ src/ValueContainer.ts
@@ -2,7 +2,7 @@ import { Observable } from "rxjs";
 
 export default interface ValueContainer<TValue> {
   readonly value: TValue;
-  value$: Observable<TValue>;
+  readonly value$: Observable<TValue>;
 }
 
 export function withValueContainer<TValue>(valueContainer: ValueContainer<TValue>) {
