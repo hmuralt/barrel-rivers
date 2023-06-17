6ec2afcd1f34b6644e75d97c42b82ba04b30303b
Heinrich Muralt <heinrich@supertext.ch>
2023-06-17 14:46:21 +0200

    Extend barrel export with AsyncLoadableState

--- src/index.ts
+++ src/index.ts
@@ -16,3 +16,4 @@ export {
 export { subState } from "./SubState";
 export { mergedState } from "./MergedState";
 export { default as ValueContainer, withValueContainer } from "./ValueContainer";
+export { default as AsyncLoadableState, SetStatus, OverallSetStatus } from "./AsyncLoadableState";
